efa044775fd0039c711a9ab8f3b971045206c1a2
ivy <597055914@qq.com>
2015-03-31 20:20:06 +0800

    update

---
+++ XiaoCaiWebApp/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="extension/jquery.mobile-1.4.5.min.css" />
+	<script type="text/javascript" src="extension/Jquery-2.1.3.min.js"></script>
+	<script type="text/javascript" src="extension/jquery.mobile-1.4.5.min.js"></script>
+	<title>晓菜</title>
+</head>
+
+<body>
+	<div data-role="page"> 
+		<div data-role="header" data-position="fixed" data-fullscreen="true">
+			<a href="index.html" data-icon="back" data-iconpos="notext"></a>
+			<div class="ui-grid-b">
+				<h1>header</h1>
+				<input type="search" name="search" id="search" placeholder="食谱 食材 工具 话题">
+   			</div>
+		</div>
+
+		<div data-role="content"><br><br>
+			<p>page content here</p>
+		</div>
+
+		<div data-role="footer" data-position="fixed" data-fullscreen="true">
+			<h1>footer</h1>
+		</div>
+	</div> 
+</body>
+
+</html>

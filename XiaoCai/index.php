de33e59ad1650e87fa6503ca311cf6064f5db822
ivy <597055914@qq.com>
2015-04-01 16:39:48 +0800

    update

--- XiaoCai/index.php
+++ XiaoCai/index.php
@@ -1,5 +1,23 @@
 <?php require('header.php'); ?>
 
+<header>
+	<nav>
+		<div class="nav-content">
+			<ul>
+				<li class="nav-menu">
+					<span class="glyphicon glyphicon-list" aria-hidden="true"></span>
+				</li>
+			</ul>
+		</div>
+	</nav>
+</header>
 
+<content>
+
+</content>
+
+<footer>
+	
+</footer>
 
 <?php require('footer.php') ?>

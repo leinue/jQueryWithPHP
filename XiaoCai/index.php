09bd7dd829c9d2fbbd86ad9a5f0fdf155384427a
ivy <597055914@qq.com>
2015-04-01 16:57:23 +0800

    update header

--- XiaoCai/index.php
+++ XiaoCai/index.php
@@ -4,9 +4,9 @@
 	<nav>
 		<div class="nav-content">
 			<ul>
-				<li class="nav-menu">
-					<span class="glyphicon glyphicon-list" aria-hidden="true"></span>
-				</li>
+				<li class="nav-menu"><span class="glyphicon glyphicon-list" aria-hidden="true"></span></li>
+				<li class="main-title">晓菜</li>
+				<li class="search-form"><input type="search" placeholder="食谱 食材 工具 话题" /></li>
 			</ul>
 		</div>
 	</nav>

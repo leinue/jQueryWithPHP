6e51d4e425ee4f510d085d67628279f1882e8393
ivy <597055914@qq.com>
2015-04-16 15:50:29 +0800

    update

--- XiaoCai/register.php
+++ XiaoCai/register.php
@@ -22,9 +22,9 @@
 			<li id="setting-list-phone-num-input">
 				<input placeholder="手机号" />
 				<a class="button button-caution button-pill button-small send-ver-code">发送验证码</a></li>
-			<li id="setting-list-password-o-input"><input placeholder="原密码" /></li>
-			<li id="setting-list-password-new-input" class="setting-list-second"><input placeholder="新密码" /></li>
-			<li id="setting-list-password-confrom-input"><input placeholder="确认新密码" /></li>
+			<li id="setting-list-password-o-input"><input placeholder="手机验证码" /></li>
+			<li id="setting-list-password-new-input" class="setting-list-second"><input type="password" placeholder="登录密码" /></li>
+			<li id="setting-list-password-confrom-input"><input type="password" placeholder="确认密码" /></li>
 		</ul>
 	</div>
 

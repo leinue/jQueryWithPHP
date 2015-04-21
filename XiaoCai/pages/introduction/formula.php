a3dcc99c7558b3198445b83f32ce17445a42209f
ivy <597055914@qq.com>
2015-04-21 13:05:06 +0800

    update

--- XiaoCai/pages/introduction/formula.php
+++ XiaoCai/pages/introduction/formula.php
@@ -9,95 +9,7 @@
 	    	加入成功
 	    </div>
 	</header>
-	<section>
-	<div class="formula-juice">
-		<div class="formula-juice-title"> 
-		<div class="juice-title">
-			<span>基础茄汁部分</span>
-			</div>
-		</div>
-		<div class="formula-juice-list">
-			<ul>
-				<li class="juice-list-li1"><span class="glyphicon glyphicon-plus-sign"></span></li>
-				<li class="juice-list-li2"><span class="glyphicon glyphicon-triangle-top"></span></li>
-				<li class="juice-list-li3">白洋菜</li>
-				<li class="juice-list-li3">1个</li>
-				<li class="juice-list-li3">切碎</li>
-			</ul>
-			<ul>
-				<li class="juice-list-li1"><span class="glyphicon glyphicon-plus-sign"></span></li>
-				<li class="juice-list-li2"><span class="glyphicon glyphicon-triangle-top"></span></li>
-				<li class="juice-list-li3">百里香</li>
-				<li class="juice-list-li3">2茶匙</li>
-				<li class="juice-list-li3">取下叶片</li>
-			</ul>
-			<ul>
-				<li class="juice-list-li1"><span class="glyphicon glyphicon-plus-sign"></span></li>
-				<li class="juice-list-li2"><span class="glyphicon glyphicon-triangle-top"></span></li>
-				<li class="juice-list-li3">罗勒</li>
-				<li class="juice-list-li3">1汤匙</li>
-				<li class="juice-list-li3">切碎</li>
-			</ul>
-			<ul>
-				<li class="juice-list-li1"><span class="glyphicon glyphicon-plus-sign"></span></li>
-				<li class="juice-list-li2"><span class="glyphicon glyphicon-triangle-top"></span></li>
-				<li class="juice-list-li3">蒜泥</li>
-				<li class="juice-list-li3">1/2汤匙</li>
-				<li class="juice-list-li3"></li>
-			</ul>
-		</div>
-	</div>
-
-
-	  <div class="formula-juice">
-		<div class="formula-juice-title"> 
-		<div class="juice-title">
-			<span>意面部分</span>
-			</div>
-		</div>
-		<div class="formula-juice-list">
-			<ul>
-				<li class="juice-list-li1"><span class="glyphicon glyphicon-plus-sign"></span></li>
-				<li class="juice-list-li2"><span class="glyphicon glyphicon-unchecked"></span></li>
-				<li class="juice-list-li3">螺旋意面</li>
-				<li class="juice-list-li3">250g</li>
-				<li class="juice-list-li3"></li>
-			</ul>
-			<ul>
-				<li class="juice-list-li1"><span class="glyphicon glyphicon-plus-sign"></span></li>
-				<li class="juice-list-li2"><span class="glyphicon glyphicon-unchecked"></span></li>
-				<li class="juice-list-li3">橄榄油</li>
-				<li class="juice-list-li3">2茶匙</li>
-				<li class="juice-list-li3"></li>
-			</ul>
-			<ul>
-				<li class="juice-list-li1"><span class="glyphicon glyphicon-plus-sign"></span></li>
-				<li class="juice-list-li2"><span class="glyphicon glyphicon-unchecked"></span></li>
-				<li class="juice-list-li3">海盐</li>
-				<li class="juice-list-li3">2汤匙</li>
-				<li class="juice-list-li3"></li>
-			</ul>
-		</div>
-	</div>
-
-
-	  <div class="formula-juice">
-		<div class="formula-juice-title"> 
-		<div class="juice-title">
-			<span>什锦海鲜茄汁面部分</span>
-			</div>
-		</div>
-		<div class="formula-juice-list">
-			<ul>
-				<li class="juice-list-li1"><span class="glyphicon glyphicon-plus-sign"></span></li>
-				<li class="juice-list-li2"><span class="glyphicon glyphicon-record"></span></li>
-				<li class="juice-list-li3">大虾</li>
-				<li class="juice-list-li3">10个</li>
-				<li class="juice-list-li3"></li>
-			</ul>
-		</div>
-	</div>
-	</section>
+	<section></section>
 
 <script type="text/javascript">
 	$(document).ready(function(){
@@ -107,6 +19,33 @@
                  }).show(300).delay(2000).hide(300);  
         }); 
       
+      	displayALertForm('正在加载...');
+      	var currentHref=document.location.href;
+      	if(currentHref.indexOf('#')!=-1){
+      		currentHref=currentHref.split('#')[1];
+      		getRecipeInfoFormula(currentHref,function(data){
+      			var jsonData=JSON.parse(data);
+      			if(jsonData['msg']!='成功'){
+      				displayALertForm(jsonData['msg']);
+      			}else{
+      				var formulaList=jsonData['data'];
+      				var formulaHTMLDOM='';
+      				for (var i = 0; i < formulaList.length; i++) {
+      					formulaChild=formulaList[i]['children'];
+      					formulaHTMLDOM+='';
+      					for (var j = 0; j < formulaChild.length; j++) {
+      						//console.log(formulaChild[j]);
+      						formulaHTMLDOM+='<ul><li class="juice-list-li1"><span class="glyphicon glyphicon-plus-sign"></span></li><li class="juice-list-li3">'+formulaChild[j]['title']+'</li><li class="juice-list-li3">'+formulaChild[j]['dosage']+'</li><li class="juice-list-li3">'+formulaChild[j]['note']+'</li></ul>';
+      					};
+      					formulaHTMLDOM='<div class="formula-juice"><div class="formula-juice-title"><div class="juice-title"><span>'+formulaList[i]['title']+'</span></div></div><div class="formula-juice-list">'+formulaHTMLDOM+'</div></div>';
+      					$('section').append(formulaHTMLDOM);
+      					formulaHTMLDOM='';
+      				};
+      			}
+      		});
+      	}else{
+      		window.location.href="recipes.php";
+      	}
     });
 
 </script>

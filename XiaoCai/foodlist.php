56bc509ea617ac84e1a5a3826a09c547fe2bca72
ivy <597055914@qq.com>
2015-04-27 20:56:41 +0800

    update

--- XiaoCai/foodlist.php
+++ XiaoCai/foodlist.php
@@ -4,7 +4,7 @@
 	<nav>
 		<div class="nav-content">
 			<ul>
-				<li class="nav-menu"><span class="glyphicon glyphicon-list" aria-hidden="true"></span></li>
+				<li class="header-back"><span class="glyphicon glyphicon-menu-left"></span></li>
 				<li class="foodlist-menu">购物清单</li>
 				<li class="delete-all">清空</li>
 			</ul>
@@ -12,5 +12,46 @@
 	</nav>
 </header>
 
+<section style="padding-top:1px">
+	<div class="setting-list food-list">
+		<ul>
+			<li>
+				<div class="food-list-title">
+					香酥炸鸡配手工酸奶酱
+					<span class="glyphicon glyphicon-menu-down"></span>
+					<span class="glyphicon glyphicon-remove"></span>
+				</div>
+				<div class="setting-list food-list-detail">
+					<ul>
+						<li>
+							<div>
+								白羊葱
+								<span>1汤勺</span>
+							</div>
+						</li>
+						<li><div class="food-list-detail-name">蒜泥<span>1/2茶勺</span></div></li>
+					</ul>
+				</div>
+			</li>
+			<li>手工草莓百香果酱<span class="glyphicon glyphicon-menu-right"></span><span class="glyphicon glyphicon-remove"></span></li>
+			<li>香酥炸鸡配手工酸奶酱<span class="glyphicon glyphicon-menu-right"></span><span class="glyphicon glyphicon-remove"></span></li>
+			<li>香酥炸鸡配手工酸奶酱<span class="glyphicon glyphicon-menu-right"></span><span class="glyphicon glyphicon-remove"></span></li>
+			<li>香酥炸鸡配手工酸奶酱<span class="glyphicon glyphicon-menu-right"></span><span class="glyphicon glyphicon-remove"></span></li>
+			<li>香酥炸鸡配手工酸奶酱<span class="glyphicon glyphicon-menu-right"></span><span class="glyphicon glyphicon-remove"></span></li>
+		</ul>
+	</div>
+
+	<div class="loading">
+		<div class="loading-main"><span class="glyphicon glyphicon-option-horizontal"></span><span class="glyphicon glyphicon-option-horizontal"></span></div>
+	</div>
+</section>
+
+<script type="text/javascript">
+	
+</script>
 
 <?php require('footer.php'); ?>
+
+<script>
+	$('.main-footer').hide();
+</script>

14517a2e604e41a53304ac0007c86f6686f7b58f
ivy <597055914@qq.com>
2015-05-01 17:05:05 +0800

    update

--- XiaoCai/curl/foodlist.php
+++ XiaoCai/curl/foodlist.php
@@ -4,7 +4,7 @@ require('base.php');
 
 $token_id=$_POST['token_id'];
 $url="http://114.215.189.210/api.php/Api/Recipe/foodList";
-$post_data=array("type"=>$type,"token_id"=>$token_id);
+$post_data=array("token_id"=>$token_id);
 $oupput=curlPost($url,$post_data);
 print_r($oupput);
 
--- XiaoCai/foodlist.php
+++ XiaoCai/foodlist.php
@@ -14,43 +14,43 @@
 
 <section style="padding-top:1px">
 	<div class="setting-list food-list">
-		<ul>
+		<ul mainmenucontainer>
 			<li>
-				<div class="food-list-title">
+				<div mainmenu class="food-list-title">
 					<a href="javascript:void(0)">香酥炸鸡配手工酸奶酱</a>
 					<span class="glyphicon glyphicon-menu-right"></span>
 					<span class="glyphicon glyphicon-remove"></span>
 				</div>
 				<div class="setting-list food-list-detail">
 					<ul>
-						<li><div>白羊葱<span>1汤勺</span></div></li>
-						<li><div class="food-list-detail-name">蒜泥<span>1/2茶勺</span></div></li>
+						<li childmenu><div>白羊葱<span>1汤勺</span></div></li>
+						<li childmenu><div class="food-list-detail-name">蒜泥<span>1/2茶勺</span></div></li>
 					</ul>
 				</div>
 			</li>
 			<li>
-				<div class="food-list-title">
+				<div mainmenu class="food-list-title">
 					<a href="javascript:void(0)">香酥炸鸡配手工酸奶酱</a>
 					<span class="glyphicon glyphicon-menu-right"></span>
 					<span class="glyphicon glyphicon-remove"></span>
 				</div>
 				<div class="setting-list food-list-detail">
 					<ul>
-						<li><div>白羊葱<span>1汤勺</span></div></li>
-						<li><div class="food-list-detail-name">蒜泥<span>1/2茶勺</span></div></li>
+						<li childmenu><div>白羊葱<span>1汤勺</span></div></li>
+						<li childmenu><div class="food-list-detail-name">蒜泥<span>1/2茶勺</span></div></li>
 					</ul>
 				</div>
 			</li>
 			<li>
-				<div class="food-list-title">
+				<div mainmenu class="food-list-title">
 					<a href="javascript:void(0)">香酥炸鸡配手工酸奶酱</a>
 					<span class="glyphicon glyphicon-menu-right"></span>
 					<span class="glyphicon glyphicon-remove"></span>
 				</div>
 				<div class="setting-list food-list-detail">
 					<ul>
-						<li><div>白羊葱<span>1汤勺</span></div></li>
-						<li><div class="food-list-detail-name">蒜泥<span>1/2茶勺</span></div></li>
+						<li childmenu><div>白羊葱<span>1汤勺</span></div></li>
+						<li childmenu><div class="food-list-detail-name">蒜泥<span>1/2茶勺</span></div></li>
 					</ul>
 				</div>
 			</li>
@@ -63,7 +63,58 @@
 </section>
 
 <script type="text/javascript">
-	$('.food-list-title:not(span)').click(function(){
+
+	getFoodList(localStorage.tokenID,function(data){
+		if(!data){
+			displayALertForm('读取出错,请重试');
+		}else{
+			var jsondata=JSON.parse(data);
+			if(!jsondata['msg']=='成功'){
+				displayALertForm(jsondata['msg'])
+			}else{
+				displayALertForm('读取成功,正在加载...');
+				var foodList=jsondata['data'];
+				foodList.forEach(function(e){
+					var recipeID=e['recipe_id'];
+					var recipeTitle=e['recipe_title'];
+					var recipeChildren=e['children'];
+					var mainMenuDOM='<div mainmenu onclick="toggleRecipeMenu(this)" idata="'+recipeID+'" class="food-list-title"><a href="javascript:void(0)">'+recipeTitle+'</a><span class="glyphicon glyphicon-menu-right"></span><span class="glyphicon glyphicon-remove"></span></div>'
+					var childMenuDom;
+					recipeChildren.forEach(function(child){
+						console.log(child);
+						var childID=child['recipe_id'];
+						var formulaID=child['formula_id'];
+						var childTitle=child['title'];
+						var dosage=child['dosage'];
+						var status=child['status'];
+						childMenuDom+='<li childmenu="true"><div>'+childTitle+'<span>'+dosage+'</span></div></li>';
+						if(childMenuDom.substring(0,9)=="undefined"){
+							childMenuDom=childMenuDom.substring(9,childMenuDom.length);
+						}	
+					});
+					childMenuDom='<div class="setting-list food-list-detail"><ul>'+childMenuDom+'</ul></div>';
+					mainMenuDOM='<li>'+mainMenuDOM+childMenuDom+'</li>';
+					$('.food-list ul[mainmenucontainer]').append(mainMenuDOM);
+					mainMenuDOM='';
+				});
+			}
+		}
+	});
+
+	function toggleRecipeMenu(obj){
+		var this_=$(obj);
+		this_.next().slideToggle();
+		this_.find('a').toggleClass('food-list-active');
+		this_.find('span:last-child').toggle();
+		var fthspan=this_.find('span:nth-child(2)');
+		if(fthspan.attr('class')=='glyphicon glyphicon-menu-down'){
+			fthspan.attr('class','glyphicon glyphicon-menu-right');
+		}else{
+			fthspan.attr('class','glyphicon glyphicon-menu-down');		
+		}
+	}
+
+	$('.food-list ul li div[mainmenu]').on('click',function(){
 		var this_=$(this);
 		this_.next().slideToggle();
 		this_.find('a').toggleClass('food-list-active');
@@ -74,6 +125,10 @@
 		}else{
 			fthspan.attr('class','glyphicon glyphicon-menu-down');		
 		}
+	});
+
+	$('.food-list-detail ul li[childmenu]').click(function(){
+
 	});
 </script>
 

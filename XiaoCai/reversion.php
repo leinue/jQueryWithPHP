cc5c79d7f487b3bfab5f1b62421164f2787010de
ivy <597055914@qq.com>
2015-05-03 11:03:33 +0800

    update

--- XiaoCai/reversion.php
+++ XiaoCai/reversion.php
@@ -13,22 +13,27 @@
 	<div class="reversion-page-content">
 		
 	</div>
-	<!--<div class="reading-list-a reversion-list-a reversion-new">
+	<div class="reading-list-a reversion-list-a reversion-new">
 		<div class="reading-list-img reversion-list-img">
 			<img src="http://www.webmaster5u.com/upfiles/file/201107/20110723234012108.jpg">
 		</div>
-		<div class="reading-list-all-content">
-			<div class="reading-list-all-title">
-				<p>菜谱炸鸡配酸奶酱菜谱炸鸡配酸配酸奶</p>
+		<div class="reversion-whole">
+			<div class="reversion-list-all-content">
+				<div class="reversion-list-all-title">
+					<p>菜谱炸鸡配酸奶酱菜谱炸鸡配酸配酸奶</p>
+				</div>
+			</div>
+			<div class="reading-list-all-foot">
+				<div class="reversion-list-foot-table">
+					<ul>
+						<li><span class="glyphicon glyphicon-bookmark"></span> 食谱</li>
+						<li><span class="glyphicon glyphicon-time"></span> 2015-05-03</li>
+					</ul>
+				</div>
 			</div>
 		</div>
-		<div class="reading-list-all-footer">
-			<ul>
-				<li><span class="glyphicon glyphicon-bookmark"></span> 食谱</li>
-				<li><span class="glyphicon glyphicon-time"></span> 今天</li>
-			</ul>
-		</div>
-	</div>-->
+		<div class="reversion-right">NEW</div>
+	</div>
 
 	<div class="loading">
 		<div class="loading-main"><span class="glyphicon glyphicon-option-horizontal"></span><span class="glyphicon glyphicon-option-horizontal"></span></div>
@@ -44,7 +49,7 @@
 	});
 
 	displayALertForm('正在加载...');
-	getReply(localStorage.tokenID,function(data){
+	/*getReply(localStorage.tokenID,function(data){
 		if(data!=''){
 			var jsonData=JSON.parse(data);
 			var replyList=jsonData['data'];
@@ -53,10 +58,12 @@
 				for (var i = 0; i < replyList.length; i++) {
 					if(replyList[i]['status']==='1'){
 						//新信息
-						replyListHTMLDom+='<div class="reading-list-a reversion-list-a reversion-new"><div style="background:url('+replyList[i]['image']+') no-repeat scroll center center transparent;background-size:cover;" class="reading-list-img reversion-list-img"></div><div class="reading-list-all-content"><div class="reading-list-all-title reversion-looked"><p>'+replyList[i]['article_title']+'</p></div></div><div class="reading-list-all-footer"><ul><li><span class="glyphicon glyphicon-bookmark"></span> '+replyType[parseInt(replyList[i]['type'])-1]+'</li><li><span class="glyphicon glyphicon-time"></span> '+replyList[i]['created_time'].split(' ')[0]+'</li></ul></div></div>';
+						console.log(replyList[i]);
+						replyListHTMLDom+='<div class="reading-list-a reversion-list-a reversion-new"><div style="background:url('+replyList[i]['article_image']+') no-repeat scroll center center transparent;background-size:cover;" class="reading-list-img reversion-list-img"></div><div class="reading-list-all-content"><div class="reading-list-all-title reversion-looked"><p>'+replyList[i]['article_title']+'</p></div></div><div class="reading-list-all-footer"><ul><li><span class="glyphicon glyphicon-bookmark"></span> '+replyType[parseInt(replyList[i]['type'])-1]+'</li><li><span class="glyphicon glyphicon-time"></span> '+replyList[i]['created_time'].split(' ')[0]+'</li></ul></div></div>';
 					}else{
 						//已读信息
-						replyListHTMLDom+='<div class="reading-list-a reversion-list-a"><div style="background:url('+replyList[i]['image']+') no-repeat scroll center center transparent;background-size:cover;" class="reading-list-img reversion-list-img"></div><div class="reading-list-all-content"><div class="reading-list-all-title reversion-looked"><p>'+replyList[i]['article_title']+'</p></div></div><div class="reading-list-all-footer"><ul><li><span class="glyphicon glyphicon-bookmark"></span> '+replyType[parseInt(replyList[i]['type'])-1]+'</li><li><span class="glyphicon glyphicon-time"></span> '+replyList[i]['created_time'].split(' ')[0]+'</li></ul></div></div>';
+						console.log(replyList[i]['article_image']);
+						replyListHTMLDom+='<div class="reading-list-a reversion-list-a"><div style="background:url('+replyList[i]['article_image']+') no-repeat scroll center center transparent;background-size:cover;" class="reading-list-img reversion-list-img"></div><div class="reading-list-all-content"><div class="reading-list-all-title reversion-looked"><p>'+replyList[i]['article_title']+'</p></div></div><div class="reading-list-all-footer"><ul><li><span class="glyphicon glyphicon-bookmark"></span> '+replyType[parseInt(replyList[i]['type'])-1]+'</li><li><span class="glyphicon glyphicon-time"></span> '+replyList[i]['created_time'].split(' ')[0]+'</li></ul></div></div>';
 					}
 				};	
 			}else{
@@ -67,7 +74,7 @@
 		}else{
 			displayALertForm('获取失败,请重试');
 		}
-	});
+	});*/
 
 	$('section').css('marginTop',$('header').height()+10);
 </script>
